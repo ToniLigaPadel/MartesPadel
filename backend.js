49568b12eb895d057834029a37680b25d287169c
ToniLigaPadel <toniligapadel@gmail.com>
2026-05-12 16:22:30 +0000

    Actualización de backend.js desde Colab

--- backend.js
+++ backend.js
@@ -21,26 +21,6 @@ const appData = {
         {
             "id_partido": 1,
             "fecha": "2026-05-12"
-        },
-        {
-            "id_partido": 2,
-            "fecha": "2026-05-12"
-        },
-        {
-            "id_partido": 3,
-            "fecha": "2026-05-12"
-        },
-        {
-            "id_partido": 4,
-            "fecha": "2026-05-12"
-        },
-        {
-            "id_partido": 5,
-            "fecha": "2026-05-12"
-        },
-        {
-            "id_partido": 6,
-            "fecha": "2026-05-12"
         }
     ],
     "parejas": [
@@ -53,42 +33,12 @@ const appData = {
             "id_pareja": 2,
             "id_jugador1": 3,
             "id_jugador2": 4
-        },
-        {
-            "id_pareja": 3,
-            "id_jugador1": 1,
-            "id_jugador2": 4
-        },
-        {
-            "id_pareja": 4,
-            "id_jugador1": 2,
-            "id_jugador2": 3
-        },
-        {
-            "id_pareja": 5,
-            "id_jugador1": 1,
-            "id_jugador2": 3
-        },
-        {
-            "id_pareja": 6,
-            "id_jugador1": 2,
-            "id_jugador2": 4
         }
     ],
     "resultados": [
         {
             "id_resultado": 1,
             "id_partido": 1,
-            "equipo_ganador": 1
-        },
-        {
-            "id_resultado": 2,
-            "id_partido": 2,
-            "equipo_ganador": 0
-        },
-        {
-            "id_resultado": 3,
-            "id_partido": 3,
             "equipo_ganador": 1
         }
     ],
@@ -105,28 +55,7 @@ const appData = {
             "id_partido": 1,
             "numero_set": 2,
             "juegos_equipo1": 6,
-            "juegos_equipo2": 2
-        },
-        {
-            "id_set": 3,
-            "id_partido": 2,
-            "numero_set": 1,
-            "juegos_equipo1": 4,
-            "juegos_equipo2": 4
-        },
-        {
-            "id_set": 4,
-            "id_partido": 3,
-            "numero_set": 1,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 1
-        },
-        {
-            "id_set": 5,
-            "id_partido": 3,
-            "numero_set": 2,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 0
+            "juegos_equipo2": 3
         }
     ],
     "partido_pareja": [
@@ -140,66 +69,6 @@ const appData = {
             "id_partido_pareja": 2,
             "id_partido": 1,
             "id_pareja": 2,
-            "equipo": 2
-        },
-        {
-            "id_partido_pareja": 3,
-            "id_partido": 2,
-            "id_pareja": 3,
-            "equipo": 1
-        },
-        {
-            "id_partido_pareja": 4,
-            "id_partido": 2,
-            "id_pareja": 4,
-            "equipo": 2
-        },
-        {
-            "id_partido_pareja": 5,
-            "id_partido": 3,
-            "id_pareja": 5,
-            "equipo": 1
-        },
-        {
-            "id_partido_pareja": 6,
-            "id_partido": 3,
-            "id_pareja": 6,
-            "equipo": 2
-        },
-        {
-            "id_partido_pareja": 7,
-            "id_partido": 4,
-            "id_pareja": 3,
-            "equipo": 1
-        },
-        {
-            "id_partido_pareja": 8,
-            "id_partido": 4,
-            "id_pareja": 4,
-            "equipo": 2
-        },
-        {
-            "id_partido_pareja": 9,
-            "id_partido": 5,
-            "id_pareja": 5,
-            "equipo": 1
-        },
-        {
-            "id_partido_pareja": 10,
-            "id_partido": 5,
-            "id_pareja": 6,
-            "equipo": 2
-        },
-        {
-            "id_partido_pareja": 11,
-            "id_partido": 6,
-            "id_pareja": 5,
-            "equipo": 1
-        },
-        {
-            "id_partido_pareja": 12,
-            "id_partido": 6,
-            "id_pareja": 6,
             "equipo": 2
         }
     ]};
