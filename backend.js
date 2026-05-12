7e8b6b45fcef761d6fd1dcaa6c098bcaf6ea0339
ToniLigaPadel <toniligapadel@gmail.com>
2026-05-12 16:42:06 +0000

    Actualización de backend.js desde Colab

--- backend.js
+++ backend.js
@@ -6,7 +6,7 @@ const appData = {
         },
         {
             "id_jugador": 2,
-            "nombre": "LUIS"
+            "nombre": "TONI"
         },
         {
             "id_jugador": 3,
@@ -14,16 +14,12 @@ const appData = {
         },
         {
             "id_jugador": 4,
-            "nombre": "TONI"
+            "nombre": "LUIS"
         }
     ],
     "partidos": [
         {
             "id_partido": 1,
-            "fecha": "2026-05-12"
-        },
-        {
-            "id_partido": 2,
             "fecha": "2026-05-12"
         }
     ],
@@ -37,41 +33,10 @@ const appData = {
             "id_pareja": 2,
             "id_jugador1": 3,
             "id_jugador2": 4
-        },
-        {
-            "id_pareja": 3,
-            "id_jugador1": 1,
-            "id_jugador2": 3
-        },
-        {
-            "id_pareja": 4,
-            "id_jugador1": 2,
-            "id_jugador2": 4
         }
     ],
-    "resultados": [
-        {
-            "id_resultado": 1,
-            "id_partido": 1,
-            "equipo_ganador": 1
-        }
-    ],
-    "sets": [
-        {
-            "id_set": 1,
-            "id_partido": 1,
-            "numero_set": 1,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 1
-        },
-        {
-            "id_set": 2,
-            "id_partido": 1,
-            "numero_set": 2,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 3
-        }
-    ],
+    "resultados": [],
+    "sets": [],
     "partido_pareja": [
         {
             "id_partido_pareja": 1,
@@ -83,18 +48,6 @@ const appData = {
             "id_partido_pareja": 2,
             "id_partido": 1,
             "id_pareja": 2,
-            "equipo": 2
-        },
-        {
-            "id_partido_pareja": 3,
-            "id_partido": 2,
-            "id_pareja": 3,
-            "equipo": 1
-        },
-        {
-            "id_partido_pareja": 4,
-            "id_partido": 2,
-            "id_pareja": 4,
             "equipo": 2
         }
     ]};
