25f72996201bf586b60eb051c2d1df13291e6a6d
ToniLigaPadel <toniligapadel@gmail.com>
2026-05-12 16:34:03 +0000

    Actualización de backend.js desde Colab

--- backend.js
+++ backend.js
@@ -21,6 +21,10 @@ const appData = {
         {
             "id_partido": 1,
             "fecha": "2026-05-12"
+        },
+        {
+            "id_partido": 2,
+            "fecha": "2026-05-12"
         }
     ],
     "parejas": [
@@ -32,6 +36,16 @@ const appData = {
         {
             "id_pareja": 2,
             "id_jugador1": 3,
+            "id_jugador2": 4
+        },
+        {
+            "id_pareja": 3,
+            "id_jugador1": 1,
+            "id_jugador2": 3
+        },
+        {
+            "id_pareja": 4,
+            "id_jugador1": 2,
             "id_jugador2": 4
         }
     ],
@@ -69,6 +83,18 @@ const appData = {
             "id_partido_pareja": 2,
             "id_partido": 1,
             "id_pareja": 2,
+            "equipo": 2
+        },
+        {
+            "id_partido_pareja": 3,
+            "id_partido": 2,
+            "id_pareja": 3,
+            "equipo": 1
+        },
+        {
+            "id_partido_pareja": 4,
+            "id_partido": 2,
+            "id_pareja": 4,
             "equipo": 2
         }
     ]};
